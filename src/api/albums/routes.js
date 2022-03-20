bffca4f1ea1f64d778737c907daabcd9ada09d8f
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-03-20 09:58:05 +0700

    update routes album

--- src/api/albums/routes.js
+++ src/api/albums/routes.js
@@ -1,24 +1,23 @@
-// untuk masalah routing akan disimpan di dalam routes.js
 const routes = (handler) => [
   {
     method: 'POST',
     path: '/albums',
-    handler: handler.postAlbumHandler, // postNoteHandler hanya menerima dan menyimpan "satu" album.
+    handler: handler.postAlbumHandler, // postAlbumHandler hanya menerima dan menyimpan "satu" album.
   },
   {
     method: 'GET',
     path: '/albums',
-    handler: handler.getAlbumsHandler, // getNotesHandler mengembalikan "banyak" album.
+    handler: handler.getAlbumsHandler, // getAlbumsHandler mengembalikan "banyak" album.
   },
   {
     method: 'GET',
     path: '/albums/{id}',
-    handler: handler.getAlbumByIdHandler, // getNoteByIdHandler mengembalikan "satu" album.
+    handler: handler.getAlbumByIdHandler, // getAlbumByIdHandler mengembalikan "satu" album.
   },
   {
     method: 'PUT',
     path: '/albums/{id}',
-    handler: handler.putAlbumByIdHandler, // putNoteByIdHandler hanya menerima dan mengubah "satu" album.
+    handler: handler.putAlbumByIdHandler, // putAlbumByIdHandler hanya menerima dan mengubah "satu" album.
   },
   {
     method: 'DELETE',
