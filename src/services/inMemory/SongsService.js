7a4123de9eadc9f1d30bf5ea6128d77ce829b4e9
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-03-20 09:11:15 +0700

    update service SongsService.js

--- src/services/inMemory/SongsService.js
+++ src/services/inMemory/SongsService.js
@@ -1,8 +1,39 @@
 // bertanggung jawab untuk mengelola resource songs yang disimpan pada memory (array)
 
+const { nanoid } = require('nanoid');
+
 class SongsService {
   constructor() {
     this._songs = [];
+  }
+
+  addSong({
+    title, year, performer, genre, duration,
+  }) {
+    const id = nanoid(16);
+    const insertedAt = new Date().toISOString();
+    const updatedAt = insertedAt;
+
+    const newSong = {
+      id,
+      title,
+      year,
+      performer,
+      genre,
+      duration,
+      insertedAt,
+      updatedAt,
+    };
+
+    this._songs.push(newSong);
+
+    const isSuccess = this._songs.filter((song) => song.id === id).length > 0;
+
+    if (!isSuccess) {
+      throw new Error('Lagu gagal ditambahkan');
+    }
+
+    return id;
   }
 }
 
