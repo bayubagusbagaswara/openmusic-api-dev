e56266907411c0bbfeb38c52a91138b60027fa99
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-03-20 14:59:05 +0700

    menambahkan plugin songs dan albums ke web server hapi di server.js

--- src/server.js
+++ src/server.js
@@ -2,10 +2,14 @@
 
 const Hapi = require('@hapi/hapi');
 const songs = require('./api/songs');
+const albums = require('./api/albums');
 const SongsService = require('./services/inMemory/SongsService');
+const AlbumsService = require('./services/inMemory/AlbumsService');
 const SongsValidator = require('./validator/songs');
+const AlbumsValidator = require('./validator/albums');
 
 const init = async () => {
+  const albumsService = new AlbumsService();
   const songsService = new SongsService();
   const server = Hapi.server({
     port: 5000,
@@ -19,13 +23,21 @@ const init = async () => {
 
   // perhatikan kita punya 2 api artinya ada 2 service yakni AlbumsService dan SongsService
   // lalu bagaimana caranya mendaftarkan 2 plugin kita di server?
-  await server.register({
-    plugin: songs,
-    options: {
-      service: songsService,
-      validator: SongsValidator,
+  await server.register([
+    {
+      plugin: albums,
+      options: {
+        service: albumsService,
+        validator: AlbumsValidator,
+      },
     },
-  });
+    {
+      plugin: songs,
+      options: {
+        service: songsService,
+        validator: SongsValidator,
+      },
+    }]);
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
