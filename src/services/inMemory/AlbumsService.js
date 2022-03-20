50f88345510ac99e4b7811e68f6e5eb60598abaf
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-03-20 09:18:11 +0700

    update AlbumsService.js mengganti class error

--- src/services/inMemory/AlbumsService.js
+++ src/services/inMemory/AlbumsService.js
@@ -1,4 +1,6 @@
 const { nanoid } = require('nanoid');
+const InvariantError = require('../../exceptions/InvariantError');
+const NotFoundError = require('../../exceptions/NotFoundError');
 
 class AlbumsService {
   constructor() {
@@ -22,7 +24,7 @@ class AlbumsService {
     this._albums.push(newAlbum);
     const isSuccess = this._albums.filter((album) => album.id === id).length > 0;
     if (!isSuccess) {
-      throw new Error('Album gagal ditambahkan');
+      throw new InvariantError('Album gagal ditambahkan');
     }
     return id;
   }
@@ -36,7 +38,7 @@ class AlbumsService {
   getAlbumById(id) {
     const album = this._albums.filter((a) => a.id === id)[0];
     if (!album) {
-      throw new Error('Album tidak ditemukan');
+      throw new NotFoundError('Album tidak ditemukan');
     }
     return album;
   }
@@ -46,7 +48,7 @@ class AlbumsService {
     const index = this._albums.findIndex((album) => album.id === id);
 
     if (index === -1) {
-      throw new Error('Gagal memperbarui album. Id tidak ditemukan');
+      throw new NotFoundError('Gagal memperbarui album. Id tidak ditemukan');
     }
 
     const updatedAt = new Date().toISOString();
@@ -63,7 +65,7 @@ class AlbumsService {
   deleteAlbumById(id) {
     const index = this._albums.findIndex((album) => album.id === id);
     if (index === -1) {
-      throw new Error('Album gagal dihapus. Id tidak ditemukan');
+      throw new NotFoundError('Album gagal dihapus. Id tidak ditemukan');
     }
     this._albums.splice(index, 1);
   }
