849f97b007598cf82b69d247c60929c4f2555d36
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-03-20 15:10:22 +0700

    mengganti port dan host di environment

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 // mengimpor dotenv dan menjalankan konfigurasinya
+require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
 const songs = require('./api/songs');
@@ -12,8 +13,8 @@ const init = async () => {
   const albumsService = new AlbumsService();
   const songsService = new SongsService();
   const server = Hapi.server({
-    port: 5000,
-    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    port: process.env.PORT,
+    host: process.env.HOST,
     routes: {
       cors: {
         origin: ['*'],
