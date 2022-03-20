8a795b3565353503f1cf3827c5984df854993773
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-03-20 07:46:23 +0700

    buat class SongsService dengan properti this._songs

---
+++ src/services/inMemory/SongsService.js
@@ -0,0 +1,9 @@
+// bertanggung jawab untuk mengelola resource songs yang disimpan pada memory (array)
+
+class SongsService {
+  constructor() {
+    this._songs = [];
+  }
+}
+
+module.exports = SongsService;
