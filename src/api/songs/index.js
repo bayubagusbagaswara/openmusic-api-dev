c5f861f14676e550a30977e4b08f6accb91131dd
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-03-20 10:01:06 +0700

    menambahkan index.js api songs

--- src/api/songs/index.js
+++ src/api/songs/index.js
@@ -1,1 +1,11 @@
-// untuk pluginnya sendiri akan disimpan pada index.js
+const SongsHandler = require('./handler');
+const routes = require('./routes');
+
+module.exports = {
+  name: 'songs',
+  version: '1.0.0',
+  register: async (server, { service, validator }) => {
+    const songsHandler = new SongsHandler(service, validator);
+    server.route(routes(songsHandler));
+  },
+};
